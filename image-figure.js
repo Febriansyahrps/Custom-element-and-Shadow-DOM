64f0d7feb63e0d45ad55f10fec577e306ec4d7fc
Febriansyah Rizqi Putra Setiyadi <41566330+Febriansyahrps@users.noreply.github.com>
2021-02-23 23:52:38 +0700

    Add files via upload

---
+++ image-figure.js
@@ -0,0 +1,7 @@
+const imageFigureElement = document.createElement("image-figure");
+
+imageFigureElement.setAttribute("src", "https://i.imgur.com/iJq78XH.jpg")
+imageFigureElement.setAttribute("alt", "dicoding")
+imageFigureElement.setAttribute("caption", "Ini Shadow DOM")
+
+document.body.appendChild(imageFigureElement)
